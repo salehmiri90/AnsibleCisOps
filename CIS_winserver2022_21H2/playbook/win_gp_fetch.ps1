c018b383b646538b5d93b8285fbb2045268928d3
Saleh Miri <52816859+salehmiri90@users.noreply.github.com>
2023-11-29 15:37:18 +0330

    Delete CIS_winserver2022_21H2/playbook/win_gp_fetch.ps1

--- CIS_winserver2022_21H2/playbook/win_gp_fetch.ps1
+++
@@ -1,5 +0,0 @@
-$policies = Get-CimInstance -Namespace "root\Microsoft\Windows\GroupPolicy" -ClassName "RSOP_PolicySetting"
-
-$policies | Select-Object -Property Setting, Precedence, Value | Format-Table
-
-
